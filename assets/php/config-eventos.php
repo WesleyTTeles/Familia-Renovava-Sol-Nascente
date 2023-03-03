5ff49cc540be355483af9c89f377e44039f54a1b
wesley teles <wesley.teles.dev@gmail.com>
2023-03-02 22:02:17 -0300

    Criando PHP mailer

---
+++ assets/php/config-eventos.php
@@ -0,0 +1,5 @@
+<?php
+define("EMAIL_ORIGEM","eventos@familiarenovadasolnascente.com.br");
+define("SENHA_EMAIL","@Eventos321!");
+define("EMAIL_DESTINO", "eventos@familiarenovadasolnascente.com.br");
+?>
---
+++ assets/php/config-familia-acao.php
@@ -0,0 +1,5 @@
+<?php
+define("EMAIL_ORIGEM","familiaemacao@familiarenovadasolnascente.com.br");
+define("SENHA_EMAIL","@Familiaemacao321!");
+define("EMAIL_DESTINO", "familiaemacao@familiarenovadasolnascente.com.br");
+?>
